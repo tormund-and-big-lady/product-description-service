bed3675047da641b59fcb235b0dc2b506fbe7c42
Matthew Mata <matthewmata1030@gmail.com>
2019-05-15 15:14:32 -0700

    init setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client/src');
+const DIST_DIR = path.join(__dirname, 'client/dist');
+const combinedLoaders = require('webpack-combine-loaders')
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: combinedLoaders([
+          {
+            loader: 'style-loader'
+          }, 
+          {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[loader]__[hash:base64:5]'
+            }
+          }
+        ])
+      }
+    ]
+  }
+}
