3cabe3f095ceb31902bb44771729e82d412d22d2
Matthew Mata <matthewmata1030@gmail.com>
2019-05-29 20:37:04 -0700

    added heroku and atlus db

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 // Connecting our local storage db to our application with the db name productDescription
-const db = mongoose.connect('mongodb://localhost/productDescription', {useNewUrlParser: true});
+const db = mongoose.connect('mongodb+srv://matthewmata1030:hackreactorfec@cluster0-esl5a.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 module.exports = db;
