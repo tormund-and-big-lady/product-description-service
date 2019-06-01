1af502544cee7423f0036fc10ebbc1ae581a1245
Matthew Mata <matthewmata1030@gmail.com>
2019-06-01 14:18:26 -0700

    fixed grammar for comments

--- server/index.js
+++ server/index.js
@@ -19,5 +19,5 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 // Router to handle all requests
 app.use('/productDescription', router);
 
-// Verifies and sets port on where server is listens at
+// Verifies and sets port on where server is listening at
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
